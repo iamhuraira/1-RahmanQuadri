8c9983bd4c0174ac1bb3fa957b78eff3556ca3eb
Abu Huraira <iamhuraira429@gmail.com>
2023-04-04 02:06:56 +0500

    first commit

---
+++ src/coponents/main.jsx
@@ -0,0 +1,12 @@
+import giphy from './assets/giphy.gif';
+import React from 'react';
+
+const Main = () => {
+    return (
+    <div className='main'>
+<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/f6UWsiU56kjWVYrsLo" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/dark-bright-lanscape-f6UWsiU56kjWVYrsLo">via GIPHY</a></p>    </div>
+    )
+}
+
+
+export default Main
